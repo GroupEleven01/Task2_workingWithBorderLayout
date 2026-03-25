6f41654f05594498e087ec142c6f40312d30a893
clementmungukende-Jakpanther <clementmungukende@gmail.com>
2026-03-25 22:30:10 +0300

    commit intergration test
    the intergration test was succesfull

---
+++ Test_Intergration/workingWithBorderLayoutTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class workingWithBorderLayoutTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void borderFrame() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void borderLabel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void borderPanel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void borderScroller() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void submitButton() {
+    }
+}
